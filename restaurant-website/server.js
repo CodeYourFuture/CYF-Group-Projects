0f7c109fdaaf088ea2ddfafc1e8dd761b7b39227
nasir89 <nasir.hematy@gmail.com>
2017-08-05 19:10:36 +0100

    I added public folder, css style, first section on home page,font awesom and nodemon library,

--- restaurant-website/server.js
+++ restaurant-website/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const exphbs = require('express-handlebars');
 const fs = require("fs");
-const readPosts = require("./helpers/readPosts")
+// const readPosts = require("./helpers/readPosts")
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -12,64 +12,60 @@ app.set('view engine', 'handlebars');
 app.use(express.static("public", { 'extensions': ['html'] }));
 
 // app.get (path, handle)
-/* app.get('/', function (req, res) {
-  res.render("index", {
-    title: 'Etzali Hernández', // insert your name instead 
-      subheading: "photography portfolio"
-
-  });
-
-}); */
-
-app.get('/my-cv', function (req, res) {
-  res.render("my-cv", {
-    //title: 'Etzalis Profile', // insert your name instead 
-    subheading: "updated cv",
-
-  });
-});
-
-app.get('/admin', function (req, res) {
-  res.render("admin", {
-    title: 'Admin', // insert your name instead 
-    subheading: "updates on admin work"
-
-  });
-});
-
-app.get("/api/posts", function (req, res) {
-  readPosts(function (error, posts) {
-    res.header("Access-Control-Allow-Origin", "*"); // permision same origin policy
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.json(posts);
-  });
-});
-
-app.get('/contact', function (req, res) {
-  res.render("contact", {
-    title: 'Get in Touch', // insert your name instead 
-    subheading: "fill the form"
-  });
-});
-
 app.get('/', function (req, res) {
-  const filePath = __dirname + '/data/posts.json';
-  const callbackFunction = function (error, file) {
-    // we call .toString() to turn the file buffer to a String 
-    const fileData = file.toString();
-    // we use JSON.parse to get an object out the String 
-    const postsJson = JSON.parse(fileData).reverse();
-    // send the json to the Template to render 
+  res.render("index");
 
-    res.render('index',
-      {
-        title: 'Etzali Profile',
-        // insert your name instead 
-        posts: postsJson,
-      });
-  };
-  fs.readFile(filePath, callbackFunction);
-});
+}); 
+
+// app.get('/my-cv', function (req, res) {
+//   res.render("my-cv", {
+//     //title: 'Etzalis Profile', // insert your name instead 
+//     subheading: "updated cv",
+
+//   });
+// });
+
+// app.get('/admin', function (req, res) {
+//   res.render("admin", {
+//     title: 'Admin', // insert your name instead 
+//     subheading: "updates on admin work"
+
+//   });
+// });
+
+// app.get("/api/posts", function (req, res) {
+//   readPosts(function (error, posts) {
+//     res.header("Access-Control-Allow-Origin", "*"); // permision same origin policy
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.json(posts);
+//   });
+// });
+
+// app.get('/contact', function (req, res) {
+//   res.render("contact", {
+//     title: 'Get in Touch', // insert your name instead 
+//     subheading: "fill the form"
+//   });
+// });
+
+// app.get('/', function (req, res) {
+//   const filePath = __dirname + '/data/posts.json';
+//   const callbackFunction = function (error, file) {
+//     // we call .toString() to turn the file buffer to a String 
+//     const fileData = file.toString();
+//     // we use JSON.parse to get an object out the String 
+//     const postsJson = JSON.parse(fileData).reverse();
+//     // send the json to the Template to render 
+
+//     res.render('index',
+//       {
+//         title: 'Etzali Profile',
+//         // insert your name instead 
+//         posts: postsJson,
+//       });
+//   };
+//   fs.readFile(filePath, callbackFunction);
+// });
 
 
 
