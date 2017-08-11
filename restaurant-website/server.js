5be9d27c0d96c95c87dacbbb76eb3290b62ad443
habiiba12 <habiiba12@hotmail.com>
2017-08-11 18:10:06 +0100

    adding menu

--- restaurant-website/server.js
+++ restaurant-website/server.js
@@ -80,6 +80,9 @@ app.get('/', function (req, res) {
 
   });
 });
+app.get("/menu", function (req, res) {
+  res.render("menu");
+})
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
