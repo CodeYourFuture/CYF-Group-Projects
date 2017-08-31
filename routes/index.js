55e63b5eb9283809c9e508e3af2fccdfb38b1aca
Nate Wright <natew@notthisway.com>
2017-08-31 17:21:28 +0100

    Use json data file for questions

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Frequenty Asked Questions',
-    description: 'Search for an answer to your question below.'
-  });
+
+  /**
+   * Define a callback function to render the
+   * homepage once the entries have been loaded
+   */
+  const renderEntries = function(error, file) {
+
+    if (error) {
+      throw error;
+    }
+
+    const fileData = file.toString();
+    const entriesData = JSON.parse(fileData);
+    res.render('index', {
+      title: 'Frequenty Asked Questions',
+      description: 'Search for an answer to your question below.',
+      entries: entriesData
+    });
+  };
+
+  /**
+   * Load the entries file
+   */
+  const entriesFilePath = __dirname + '/../data/entries.json';
+  fs.readFile(entriesFilePath, renderEntries);
 });
 
 module.exports = router;
