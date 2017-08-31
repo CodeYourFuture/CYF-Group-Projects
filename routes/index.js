86c4aae3df7b091c55af5e54b94f821b7dea9c41
Nate Wright <natew@notthisway.com>
2017-08-31 17:01:04 +0100

    Initial layout and styles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Frequenty Asked Questions',
+    description: 'Search for an answer to your question below.'
+  });
 });
 
 module.exports = router;
