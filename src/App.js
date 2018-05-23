73970c3dca35a40e53a5bd44c4659a8e214b382a
Nate Wright <natew@notthisway.com>
2018-05-23 21:09:17 +0100

    First-pass at app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
+import Favourites from './components/Favourites';
+import RandomDog from './components/RandomDog';
+import DogBattle from './components/DogBattle';
+import Breeds from './components/Breeds';
 import './App.css';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      balance: 87.43,
-    };
+      favouriteBreeds: [],
+      savedPhotos: [],
+    }
   }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Title</h1>
+          <h1 className="App-title">Dogs!</h1>
         </header>
+        <Favourites />
+        <RandomDog />
+        <DogBattle />
+        <Breeds />
       </div>
     );
   }
