bbec4eec91c93b906f59bfd17e058bb42682c17a
Luke Taylor <tekul.hs@gmail.com>
2019-02-28 20:12:49 +0000

    Reformat codebase

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.get("/", function(req, res, next) {
   /**
    * Define a callback function to render the
    * homepage once the products data has been loaded
    */
   const renderProducts = function(error, file) {
-
     if (error) {
       throw error;
     }
 
     const fileData = file.toString();
     const productsData = JSON.parse(fileData);
-    res.render('index', {
-      title: 'AcmeInc',
-      description: 'We sell the finest goods and services.',
-      products: productsData,
+    res.render("index", {
+      title: "AcmeInc",
+      description: "We sell the finest goods and services.",
+      products: productsData
     });
   };
 
   /**
    * Load the products file
    */
-  const productsFilePath = __dirname + '/../data/products.json';
+  const productsFilePath = __dirname + "/../data/products.json";
   fs.readFile(productsFilePath, renderProducts);
 });
 
