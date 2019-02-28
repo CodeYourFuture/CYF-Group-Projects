4655ce4e75a15dec7d4ce00097b316b46610c19e
Luke Taylor <tekul.hs@gmail.com>
2019-02-28 19:55:17 +0000

    Reformat codebase

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.get("/", function(req, res, next) {
   /**
    * Define a callback function to render the
    * homepage once the topics data has been loaded
    */
   const renderTopics = function(error, file) {
-
     if (error) {
       throw error;
     }
 
     const fileData = file.toString();
     const topicsData = JSON.parse(fileData);
-    res.render('index', {
-      title: 'Progress Tracker',
-      description: 'Keep track of your progress learning new topics. Add a topic below and rate your knowledge of the topic between 1 (no knowledge) and 10 (very confident).',
+    res.render("index", {
+      title: "Progress Tracker",
+      description:
+        "Keep track of your progress learning new topics. Add a topic below and rate your knowledge of the topic between 1 (no knowledge) and 10 (very confident).",
       columns: [
         {
-          label: 'Topic',
+          label: "Topic",
           hideLabel: true
         },
         {
-          label: 'Before Class',
+          label: "Before Class"
         },
         {
-          label: 'After Class',
+          label: "After Class"
         },
         {
-          label: 'Monday',
+          label: "Monday"
         },
         {
-          label: 'Wednesday',
+          label: "Wednesday"
         },
         {
-          label: 'Thursday',
+          label: "Thursday"
         },
         {
-          label: 'Friday',
+          label: "Friday"
         },
         {
-          label: 'Edit',
+          label: "Edit",
           hideLabel: true
         }
       ],
-      topics: topicsData,
+      topics: topicsData
     });
   };
 
   /**
    * Load the topics file
    */
-  const topicsFilePath = __dirname + '/../data/topics.json';
+  const topicsFilePath = __dirname + "/../data/topics.json";
   fs.readFile(topicsFilePath, renderTopics);
 });
 
