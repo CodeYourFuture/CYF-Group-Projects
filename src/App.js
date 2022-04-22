d82a2ccac2941c4afff46a114a0b9da191f62c89
ehwus <alex@organise.network>
2022-04-22 17:36:10 +0100

    Upgraded dependencies and refactored to functional components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Favourites from "./components/Favourites";
 import RandomDog from "./components/RandomDog";
 import DogBattle from "./components/DogBattle";
 import Breeds from "./components/Breeds";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      favouriteBreeds: [],
-      savedPhotos: []
-    };
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Dogs!</h1>
-        </header>
-        <Favourites />
-        <RandomDog />
-        <DogBattle />
-        <Breeds />
-      </div>
-    );
-  }
-}
+export default function App() {
+  const [favouriteBreeds, setFavouriteBreeds] = useState([]);
+  const [savedPhotos, setSavedPhotos] = useState([]);
 
-export default App;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Dogs!</h1>
+      </header>
+      <Favourites />
+      <RandomDog />
+      <DogBattle />
+      <Breeds />
+    </div>
+  );
+}
